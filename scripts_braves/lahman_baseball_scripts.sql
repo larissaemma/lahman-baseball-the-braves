92269b2ef84456650bdd80b4fca52846bd5a2fa4
AdellAD4 <adaly4444@gmail.com>
2024-03-01 12:38:05 -0600

    Last part q.7 needs work, q. 9 needs a lot of help

--- scripts_braves/lahman_baseball_scripts.sql
+++ scripts_braves/lahman_baseball_scripts.sql
@@ -56,17 +56,141 @@ GROUP BY defensive_position;
 
    
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
-   
+ 
+SELECT (FLOOR(yearid / 10) * 10) AS Decade,
+		ROUND (avg (so),2) as avg_so
+FROM public.batting
+WHERE yearid >= 1920
+GROUP BY FLOOR(yearid/ 10)
+ORDER BY decade;
+
+SELECT (FLOOR(yearid / 10) * 10) AS Decade,
+		ROUND (avg (hr),2) as avg_hr
+FROM public.batting
+WHERE yearid >= 1920
+GROUP BY FLOOR(yearid/ 10)
+ORDER BY decade;
+
+SELECT
+	sub.decade,
+	sub.num_games,
+	sub.strike_outs,
+	ROUND(SUM(sub.strike_outs) / SUM(sub.num_games), 2) AS avg_so_per_game
+FROM
+	(SELECT
+	 		SUM(g) AS num_games,
+	 		SUM(so) AS strike_outs,
+	 		CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
+				WHEN yearid BETWEEN 1930 AND 1939 THEN '1930s'
+				WHEN yearid BETWEEN 1940 AND 1949 THEN '1940s'
+				WHEN yearid BETWEEN 1950 AND 1959 THEN '1950s'
+				WHEN yearid BETWEEN 1960 AND 1969 THEN '1960s'
+				WHEN yearid BETWEEN 1970 AND 1979 THEN '1970s'
+				WHEN yearid BETWEEN 1980 AND 1989 THEN '1980s'
+				WHEN yearid BETWEEN 1990 AND 1999 THEN '1990s'
+				WHEN yearid BETWEEN 2000 AND 2009 THEN '2000s'
+				WHEN yearid BETWEEN 2010 and 2019 THEN '2010s'
+				ELSE '<=1910s' END AS decade
+	FROM pitching
+	GROUP BY decade
+	ORDER BY decade ASC) AS sub
+GROUP BY sub.num_games, sub.strike_outs, sub.decade
+ORDER BY sub.decade ASC;
 
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
-	
+
+SELECT
+	people.namefirst,
+	people.namelast,
+	sub.playerid,
+	sub.stolen_bases,
+	sub.caught_stealing,
+	sub.total_attempts,
+	ROUND(SUM(sub.stolen_bases) / SUM(sub.total_attempts), 2) AS sb_success_rate
+FROM (SELECT yearid,
+	  		playerid,
+			SUM(sb) AS stolen_bases,
+			SUM(cs) AS caught_stealing,
+			SUM(sb + cs) AS total_attempts
+		FROM batting
+		WHERE yearid = 2016
+		GROUP BY playerid, yearid
+		ORDER BY total_attempts DESC) AS sub
+JOIN people
+ON sub.playerid = people.playerid
+WHERE sub.total_attempts >= 20 AND sub.yearid = 2016
+GROUP BY people.namefirst, people.namelast, sub.playerid, sub.stolen_bases, sub.caught_stealing, 					sub.total_attempts, sub.yearid
+ORDER BY sb_success_rate DESC;
 
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+
+SELECT *
+FROM teams
+WHERE yearid >=1970 AND wswin = 'N'
+ORDER BY w DESC;
+
+--ANSWER Largest number of wins for a team that did not win the world series is 116.
+
+
+SELECT *
+FROM teams
+WHERE yearid >=1970 AND wswin = 'Y'
+ORDER BY w;
+
+--ANSWER Smallest number of wins for a team that did win the world series is 63.
+
+SELECT yearid, w, wswin
+FROM teams
+WHERE yearid >=1970 AND yearid <> 1981 AND wswin = 'Y'
+ORDER BY w;
+
+SELECT yearid, w, wswin, COUNT(teamid)
+FROM teams
+WHERE yearid >=1970 AND wswin = 'Y'
+GROUP BY yearid, w, wswin
+ORDER BY w DESC;
 
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
+SELECT ROUND(AVG(attendance/games),2) as avg_attendance, team, park, games
+FROM homegames
+GROUP BY team, park, games
+ORDER BY avg_attendance DESC;
+
+--ANSWER 65342.00	"CLE"	"CLE07"
+      -- 57569.00	"COL"	"DEN01"
+      -- 55348.00	"COL"	"DEN01"
+      -- 55000.00	"TBA"	"TOK01"
+      -- 55000.00	"NYN"	"TOK01"
+	  
+SELECT ROUND(AVG(attendance/games),2) as avg_attendance, team, park, games
+FROM homegames
+GROUP BY team, park, games
+ORDER BY avg_attendance;
+
+-- ANSWER 0.00	"CL6"	"COL02"
+       -- 0.00	"PH4"	"PHI01"
+       -- 0.00	"CHN"	"CHI06"
+       -- 0.00	"SLN"	"STL05"
+       -- 0.00	"CL2"	"CLE02"
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+SELECT *
+FROM awardsmanagers
+CASE WHEN playerid
+WHERE awardid = 'TSN Manager of the Year' AND lgid = 'NL' OR lgid = 'AL'
+ORDER BY playerid
+
+-- SELECT am.playerid, am.awardid, am.lgid
+-- FROM awardsmanagers as am
+-- INNER JOIN (
+-- 	SELECT playerid, awardid, lgid
+-- 	FROM awardsmanagers
+-- 	WHERE awardid = 'TSN Manager of the Year' AND lgid = 		'NL'
+-- 	GROUP BY playerid, awardid, lgid) as sub
+-- 	ON sub.lgid = am.lgid
+-- WHERE am.awardid = 'TSN Manager of the Year' AND am.lgid='AL'
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
