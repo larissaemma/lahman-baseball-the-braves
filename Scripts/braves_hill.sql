9043b9a90105120de1c8e3dd89cee90b7091da53
JoshHill5452 <joshhill5452@gmail.com>
2024-03-02 14:04:02 -0600

    Final Baseball Push

--- Scripts/braves_hill.sql
+++ Scripts/braves_hill.sql
@@ -20,40 +20,183 @@ LEFT JOIN teams
 ON appearances.teamid = teams.teamid
 GROUP BY people.namefirst, people.namelast, people.height, appearances.g_all, teams.name
 ORDER BY people.height ASC
-LIMIT 10;
+LIMIT 1;
 
 --ANSWER: Eddie Gaedel, 43" tall, Played in 1 game, St. Louis Browns
 
 --3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
-SELECT namefirst,
-	namelast,
-	SUM(salary) AS total_salary
+SELECT namefirst, namelast, SUM(s.salary)
 FROM people
-INNER JOIN salaries
-ON people.playerid = salaries.playerid
-INNER JOIN collegeplaying
-ON salaries.playerid = collegeplaying.playerid
-WHERE collegeplaying.schoolid LIKE 'vandy'
-GROUP BY namefirst, namelast
-ORDER BY total_salary DESC;
+	INNER JOIN salaries AS s
+		USING (playerid)
+WHERE playerid IN(SELECT DISTINCT playerid
+		FROM collegeplaying
+		WHERE schoolid = 'vandy')
+GROUP BY namefirst, namelast;
 
---ANSWER: David Price, $245,553,888
+--ANSWER: David Price, $81,851,296
 
 --4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
-SELECT *
-FROM collegeplaying
-ORDER BY schoolid DESC;
+SELECT
+	SUM(po) AS total_putouts,
+	CASE WHEN pos = 'OF' THEN 'Outfield'
+	WHEN pos = 'P' OR pos = 'C' THEN 'Battery'
+	ELSE 'Infield' END AS defensive_position
+FROM fielding
+WHERE yearid = 2016 AND pos IS NOT NULL
+GROUP BY defensive_position;
+
+--ANSWER: Battery-938, Infield-661, Outfield- 354
 
 --5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
 
+SELECT 10 * FLOOR(yearid/10) AS decade,
+		ROUND(SUM(so) / SUM(g)::NUMERIC, 2) AS avg_so_game,
+		ROUND(SUM(hr) / SUM(g)::NUMERIC, 2) AS avg_hr_game
+FROM teams
+WHERE yearid >= 1920
+GROUP BY decade
+ORDER BY decade;
+
 --6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
+
+SELECT
+	people.namefirst,
+	people.namelast,
+	sub.playerid,
+	sub.stolen_bases,
+	sub.caught_stealing,
+	sub.total_attempts,
+	ROUND(SUM(sub.stolen_bases) / SUM(sub.total_attempts)*100, 2) AS sb_success_rate
+FROM (SELECT yearid, 
+	  		playerid,
+			SUM(sb) AS stolen_bases,
+			SUM(cs) AS caught_stealing,
+			SUM(sb + cs) AS total_attempts
+		FROM batting
+		WHERE yearid = 2016
+		GROUP BY playerid, yearid
+		ORDER BY total_attempts DESC) AS sub
+JOIN people
+ON sub.playerid = people.playerid
+WHERE sub.total_attempts >= 20 AND sub.yearid = 2016
+GROUP BY people.namefirst, people.namelast, sub.playerid, sub.stolen_bases, sub.caught_stealing, 					sub.total_attempts, sub.yearid
+ORDER BY sb_success_rate DESC;
+
+--ANSWER: Chris Owings had 21 stolen bases out of 23 attempts resulting in a 91% success rate.
 
 --7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
+WITH maxwins AS
+	(SELECT yearid,
+		MAX(w) AS w
+	FROM teams
+	WHERE yearid >= 1970
+	 	AND yearid <> 1981
+	GROUP BY yearid),
+flagteams AS
+	(SELECT teams.name,
+	 	teams.wswin,
+	 	maxwins.yearid,
+	 	maxwins.w
+	FROM maxwins
+	INNER JOIN teams
+	USING (yearid, w))
+SELECT (SELECT COUNT (*)
+		FROM flagteams
+		WHERE wswin = 'Y') * 100.0/ COUNT(*)
+FROM flagteams
+
+--ANSWER:
+	--a.) 2001 SEA with 116 wins and no WS
+	--b.) 1981 LAN with 63 wins and won the World Series (player strike shortened season)
+	--c.) SEE CODE
+	--d.) One time= .02%
+
 --8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
+(SELECT 'Top 5' AS top_or_bottom,
+ 	RANK() OVER(ORDER BY SUM(homegames.attendance) / SUM(games) DESC) AS rank,
+	teams.name AS team_name,
+	park_name,
+	SUM(homegames.attendance) / SUM(games) AS avg_attendance
+FROM homegames
+LEFT JOIN parks
+USING(park)
+LEFT JOIN teams
+ON teams.teamid = homegames.team AND teams.yearid = homegames.year
+WHERE year = 2016
+	AND games >= 10
+GROUP BY teams.name, park_name	
+ORDER BY avg_attendance DESC
+LIMIT 5)
+UNION
+(SELECT 'Bottom 5',
+	RANK() OVER(ORDER BY SUM(homegames.attendance) / SUM(games)) AS rank,
+ 	teams.name,
+	park_name,
+	SUM(homegames.attendance) / SUM(games) AS avg_attendance
+FROM homegames
+LEFT JOIN parks
+USING(park)
+LEFT JOIN teams
+ON teams.teamid = homegames.team AND teams.yearid = homegames.year
+WHERE year = 2016
+	AND games >= 10
+GROUP BY teams.name, park_name	
+ORDER BY avg_attendance
+LIMIT 5)
+ORDER BY avg_attendance DESC;
+
 --9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+	
+WITH both_league_winners AS (
+	SELECT
+		playerid
+	FROM awardsmanagers
+	WHERE awardid = 'TSN Manager of the Year'
+		AND lgid IN ('AL', 'NL')
+	GROUP BY playerid
+	HAVING COUNT(DISTINCT lgid) = 2
+	)
+SELECT DISTINCT people.namefirst|| ' ' || people.namelast AS name,
+		managers.teamid,
+		managers.lgid, yearid
+FROM people
+INNER JOIN managers
+USING (playerid)
+INNER JOIN awardsmanagers
+USING (playerid, yearid)
+INNER JOIN both_league_winners
+USING (playerid)
 
 --10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
+
+WITH players2016 AS
+	(SELECT playerid,
+	 namefirst|| ' ' || namelast AS name,
+	 hr
+	FROM batting
+	INNER JOIN people
+	USING (playerid)
+		INNER JOIN (
+  		SELECT playerid, count (yearid) as yearsplayed
+  		FROM batting
+  		INNER JOIN people as p
+  		USING (playerid)
+  		GROUP BY playerid
+ 		 HAVING count (yearid)  >= 10
+		) years
+		USING (playerid)
+		WHERE yearid= 2016 AND hr > 0
+		ORDER BY hr DESC),
+rawrh as
+  		(SELECT playerid, MAX (hr) as hr
+		FROM batting
+		GROUP By playerid)
+SELECT *
+FROM players2016
+INNER JOIN rawrh
+USING (playerid, hr)
