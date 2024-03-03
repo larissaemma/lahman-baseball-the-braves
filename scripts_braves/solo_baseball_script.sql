3c56cf56a770e0f927e25071958aa86f4d9b3f02
AdellAD4 <adaly4444@gmail.com>
2024-03-02 19:39:16 -0600

    Trying to divide L CY winners by L pitchers

--- scripts_braves/solo_baseball_script.sql
+++ scripts_braves/solo_baseball_script.sql
@@ -2,32 +2,32 @@
 
 -- 13. It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
 
-SELECT playerid, namefirst || ' ' || namelast AS full_name, bats
+SELECT playerid, namefirst || ' ' || namelast AS full_name, throws
 FROM people
-WHERE bats = 'L'
---4959 Lefties
+WHERE throws = 'L'
+--3654 Lefties
 
-SELECT playerid, namefirst || ' ' || namelast AS full_name, bats
+SELECT playerid, namefirst || ' ' || namelast AS full_name, throws
 FROM people
-WHERE bats = 'R'
---11794 Righties
---16753 total
+WHERE throws = 'R'
+--14480 Righties
+--18134 total
 
 WITH lefties AS
 	(SELECT 
 	 playerid, 
 	 namefirst || ' ' || namelast AS full_name, 
-	 bats
+	 throws
 	FROM people
-	WHERE bats = 'L'
+	WHERE throws = 'L'
 	),
 	righties AS
 	(SELECT 
 	 playerid, 
 	 namefirst || ' ' || namelast AS full_name, 
-	 bats
+	 throws
 	FROM people
-	WHERE bats = 'R'
+	WHERE throws = 'R'
 	)
 SELECT ROUND(
 	(SELECT COUNT (playerid)
@@ -35,65 +35,121 @@ SELECT ROUND(
 	((SELECT COUNT (playerid)
 	FROM righties) + (SELECT COUNT (playerid)
 	FROM lefties)),2)
---29.60% Is this correct?
+--20.15% 
 
 
 WITH lefties AS
 	(SELECT 
 	 playerid, 
 	 namefirst || ' ' || namelast AS full_name, 
-	 bats
+	 throws
 	FROM people
-	WHERE bats = 'L'
+	WHERE throws = 'L'
 	)
 SELECT *
 FROM lefties
 INNER JOIN awardsplayers
 USING (playerid)
 WHERE awardid = 'Cy Young Award'
---32 times
+--37 times
+
+
+
+-- WITH left_cy_award AS
+-- 	((SELECT 
+-- 	 playerid, 
+-- 	 namefirst || ' ' || namelast AS full_name, 
+-- 	 throws
+-- 	FROM people
+-- 	WHERE throws = 'L'
+-- 	)
+-- 	SELECT *
+-- 	FROM lefties
+-- 	INNER JOIN awardsplayers
+-- 	USING (playerid)
+-- 	WHERE awardid = 'Cy Young Award')),
+	
+-- 	lefties AS
+-- 	(SELECT 
+-- 	 playerid, 
+-- 	 namefirst || ' ' || namelast AS full_name, 
+-- 	 throws
+-- 	FROM people
+-- 	WHERE throws = 'L'
+-- 	)
+	
+-- SELECT ROUND(
+-- 	SELECT COUNT (playerid)
+-- 	FROM left_cy_award)*100.0/
+-- 	((SELECT COUNT (playerid)
+-- 	 FROM lefties)),2)
+	 
+-- WITH lefties AS
+-- 	(SELECT 
+-- 	 playerid, 
+-- 	 namefirst || ' ' || namelast AS full_name, 
+-- 	 throws
+-- 	FROM people
+-- 	WHERE throws = 'L'
+-- 	)	
+-- SELECT ROUND(
+-- 	 (SELECT COUNT(WITH lefties AS
+-- 	(SELECT 
+-- 	 playerid, 
+-- 	 namefirst || ' ' || namelast AS full_name, 
+-- 	 throws
+-- 	FROM people
+-- 	WHERE throws = 'L'
+-- 	)
+-- SELECT *
+-- FROM lefties
+-- INNER JOIN awardsplayers
+-- USING (playerid)
+-- WHERE awardid = 'Cy Young Award'(playerid))*100.0/
+-- 	(SELECT COUNT (playerid)
+-- 	FROM lefties)),2)
 
 WITH righties AS
 	(SELECT 
 	 playerid, 
 	 namefirst || ' ' || namelast AS full_name, 
-	 bats
+	 throws
 	FROM people
-	WHERE bats = 'R'
+	WHERE throws = 'R'
 	)
 SELECT *
 FROM righties
 INNER JOIN awardsplayers
 USING (playerid)
 WHERE awardid = 'Cy Young Award'
---77 times
+--75 times
 
 WITH lefties AS
 	(SELECT 
 	 playerid, 
 	 namefirst || ' ' || namelast AS full_name, 
-	 bats
+	 throws
 	FROM people
-	WHERE bats = 'L'
+	WHERE throws = 'L'
 	)
 SELECT *
 FROM lefties
 INNER JOIN halloffame
 USING (playerid)
 WHERE inducted = 'Y'
---88 times
+--52 times
 
 WITH righties AS
 	(SELECT 
 	 playerid, 
 	 namefirst || ' ' || namelast AS full_name, 
-	 bats
+	 throws
 	FROM people
-	WHERE bats = 'R'
+	WHERE throws = 'R'
 	)
 SELECT *
 FROM righties
 INNER JOIN halloffame
 USING (playerid)
 WHERE inducted = 'Y'
---170 times
+--231 times
